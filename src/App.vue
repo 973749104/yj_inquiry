500a7cb25445d212bcb9de6fb1f3d7c3860efa2b
LHX <l973749104@gmail.com>
2018-01-27 14:02:54 +0800

    创建table and tabs

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <Tabs>
+      <TabPane label="iphone1">
+        <Table></Table>
+      </TabPane>
+      <TabPane label="iphone2">
+        <Table></Table>
+      </TabPane>
+      <TabPane label="iphone3">
+        <Table></Table>
+      </TabPane>
+      <TabPane label="iphone4">
+        <Table></Table>
+      </TabPane>
+      <TabPane label="iphone5">
+        <Table></Table>
+      </TabPane>
+      <TabPane label="iphone6">
+        <Table></Table>
+      </TabPane>
+      <TabPane label="iphone7">
+        <Table></Table>
+      </TabPane>
+      <TabPane label="iphone8">
+        <Table></Table>
+      </TabPane>
+      <TabPane label="iphone9">
+        <Table></Table>
+      </TabPane>
+    </Tabs>
+  </div>
+</template>
+
+<script>
+import Table from './components/tableCom';
+export default {
+  name: 'App',
+  components: {
+    Table
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/tableCom.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="tableCom">
+    <Table  ref="selection" :columns="columns" :data="dataTable" size="small"></Table>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      columns: [
+        {
+          type: 'selection',
+          width: 60,
+          align: 'center'
+        },
+        {
+          title: 'SN',
+          key: 'SN',
+          filters:[
+            { label: '3131', value: '3131'},
+            { label: '3131', value: '3131'},
+            { label: '3131', value: '3131'},
+            { label: '3131', value: '3131'},
+            { label: '3131', value: '3131'},
+            { label: '3131', value: '3131'},
+            { label: '3131', value: '3131'},
+            { label: '3131', value: '3131'},
+            { label: '3131', value: '3131'},
+            { label: '3131', value: '3131'},
+            { label: '3131', value: '3131'},
+            { label: '3131', value: '3131'},
+            { label: '3131', value: '3131'},
+            { label: '3131', value: '3131'},
+            { label: '3131', value: '3131'},
+            ]
+        },
+        {
+          title: 'IMEI',
+          key: 'IMEI'
+        },
+        {
+          title: 'ECID',
+          key: 'ECID'
+        },
+        {
+          title: 'MLBSN',
+          key: 'MLBSN'
+        }
+      ],
+      dataTable: [
+        {
+          SN: '21321',
+          IMEI: 'ASDAS',
+          ECID: 'AS25D2AS4',
+          MLBSN: '3S4DW313',
+        },
+        {
+          SN: '21321',
+          IMEI: 'ASDAS',
+          ECID: 'AS25D2AS4',
+          MLBSN: '3S4DW313',
+        },
+        {
+          SN: '21321',
+          IMEI: 'ASDAS',
+          ECID: 'AS25D2AS4',
+          MLBSN: '3S4DW313',
+        },
+        {
+          SN: '21321',
+          IMEI: 'ASDAS',
+          ECID: 'AS25D2AS4',
+          MLBSN: '3S4DW313',
+        },
+        {
+          SN: '21321',
+          IMEI: 'ASDAS',
+          ECID: 'AS25D2AS4',
+          MLBSN: '3S4DW313',
+        },
+        {
+          SN: '21321',
+          IMEI: 'ASDAS',
+          ECID: 'AS25D2AS4',
+          MLBSN: '3S4DW313',
+        },
+        {
+          SN: '21321',
+          IMEI: 'ASDAS',
+          ECID: 'AS25D2AS4',
+          MLBSN: '3S4DW313',
+        },
+        {
+          SN: '21321',
+          IMEI: 'ASDAS',
+          ECID: 'AS25D2AS4',
+          MLBSN: '3S4DW313',
+        },
+        {
+          SN: '21321',
+          IMEI: 'ASDAS',
+          ECID: 'AS25D2AS4',
+          MLBSN: '3S4DW313',
+        },
+        {
+          SN: '21321',
+          IMEI: 'ASDAS',
+          ECID: 'AS25D2AS4',
+          MLBSN: '3S4DW313',
+        },
+        {
+          SN: '21321',
+          IMEI: 'ASDAS',
+          ECID: 'AS25D2AS4',
+          MLBSN: '3S4DW313',
+        },
+        {
+          SN: '21321',
+          IMEI: 'ASDAS',
+          ECID: 'AS25D2AS4',
+          MLBSN: '3S4DW313',
+        },
+        {
+          SN: '21321',
+          IMEI: 'ASDAS',
+          ECID: 'AS25D2AS4',
+          MLBSN: '3S4DW313',
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
